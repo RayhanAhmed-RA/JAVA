b8e9546ce2b73e114586b6ba2914c29e872d4e9d
RayhanAhmed-RA <68095894+RayhanAhmed-RA@users.noreply.github.com>
2021-02-01 21:04:43 +0600

    Add files via upload

---
+++ BasicJava/src/basicjava/BioData.java
@@ -0,0 +1,31 @@
+
+package basicjava;
+
+
+public class BioData {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Name: Rayhan Ahmed");
+        System.out.println("Id: 17-33202-1");
+        System.out.println("Phone: 0267963");
+        System.out.println("Adderss: Kuril\n");
+        
+        //Escape Sequence
+        /*
+        Escape Sequence is a special character followed by backslash
+        \b      backspace
+        \t      tab
+        \n      newline
+        \r      carriage return
+        \"      double quote
+        \'      single quote
+        */
+        
+        System.out.println("Dept: CSE \nProgram: BSc Eng \nYear: 0000\n");
+        
+        System.out.println("1 \t 2");
+        System.out.println("5 \t 4");
+    }
+    
+}
